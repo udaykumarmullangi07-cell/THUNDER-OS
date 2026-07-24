71c54d89dc13d39aae77494b7ee6d0d26237b2a4
Uday Kumar <udaykumarmullangi07@gmail.com>
2026-07-24 21:29:30 +0530

    Build application shell with sidebar and navbar

--- frontend/src/components/layout/Sidebar/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import SidebarHeader from "./SidebarHeader";
 
 function Sidebar() {
   return (
-    <aside className="w-72 min-h-screen bg-[#0B1220] border-r border-zinc-800">
+    <aside className="w-80 min-h-screen bg-[#0B1220] border-r border-zinc-800">
       <SidebarHeader />
     </aside>
   );
